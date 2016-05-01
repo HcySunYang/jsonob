0b36ea0c19714b761edb3dbc5fe13ad86821c964
huochunyang1 <huochunyang1@doumi.com>
2016-05-01 20:27:30 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
+/*
+ *  Object 原型
+ */
 const OP = Object.prototype;
+/*
+ *  需要重写的数组方法 OAR 是 overrideArrayMethod 的缩写
+ */
+const OAM = ['push', 'pop', 'shift', 'unshift', 'splice', 'sort', 'reverse'];
 
 export class Jsonob{
     constructor(obj, callback){
@@ -6,11 +13,13 @@ export class Jsonob{
             console.error('This parameter must be an object：' + obj);
         }
         this.$callback = callback;
-        this.$cacheData = 
         this.observe(obj);
     }
     
     observe(obj){
+        if(OP.toString.call(obj) === '[object Array]'){
+            this.overrideArrayProto(obj);
+        }
         Object.keys(obj).forEach(function(key, index, keyArray){
             var oldVal = obj[key];
             Object.defineProperty(obj, key, {
@@ -19,7 +28,7 @@ export class Jsonob{
                 },
                 set: (function(newVal){
                     if(oldVal !== newVal){
-                        if(OP.toString.call(newVal) === '[object Object]'){
+                        if(OP.toString.call(newVal) === '[object Object]' || OP.toString.call(newVal) === '[object Array]'){
                             this.observe(newVal);
                         }
                         this.$callback(newVal, oldVal);
@@ -29,12 +38,43 @@ export class Jsonob{
                 }).bind(this)
             });
             
-            if(OP.toString.call(obj[key]) === '[object Object]'){
+            if(OP.toString.call(obj[key]) === '[object Object]' || OP.toString.call(obj[key]) === '[object Array]'){
                 this.observe(obj[key]);
             }
-            // console.log(OP.toString.call(obj[key]))
             
         }, this);
+        
+    }
+    
+    overrideArrayProto(array){
+        var originalProto = Array.prototype,
+            overrideProto = Object.create(Array.prototype),
+            self = this,
+            result;
+        
+        Object.keys(OAM).forEach(function(key, index, array){
+            var method = OAM[index],
+                oldArray = [];
+            
+            Object.defineProperty(overrideProto, method, {
+                value: function(){
+                    oldArray = this.slice(0);
+                    
+                    var arg = [].slice.apply(arguments);
+                    result = originalProto[method].apply(this, arg);
+                    
+                    self.observe(this);
+                    self.$callback(this, oldArray);
+                    
+                    return result;
+                },
+                writable: true,
+                enumerable: false,
+                configurable: true
+            });
+        }, this);
+        
+        array.__proto__ = overrideProto;
         
     }
 }
